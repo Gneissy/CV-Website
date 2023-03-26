b9e783f3939e011b77bf33fd6bad8f662df369e2
Gneissy <ahmetserteserr@gmail.com>
2023-03-26 12:05:59 +0300

    Refactoring slightly on JS and HTML file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 
 
-// Fetch contact mails
 
+gsap.registerPlugin(ScrollTrigger); // Activating ScrollTrigger
+
+
+// Fetch contact mails
 const form = document.getElementById("contact-form");
 const messageContainer = document.getElementById("message-container");
 
@@ -48,52 +51,26 @@ $(".skill-img__bottom-div").hover(function(){
 
 
 
-//
-// $(".skill-img__top-div").hover(function(){
-//   $(".skill-box").animate({
-//     opacity: 1,
-//     left: "-=10"
-//   }, 250, function(){
-//     // Animated
-//   });
-// });
-//
-// $(".skill-img__ind-wrapper").mouseleave(function(){
-//   $(".skill-box").animate({
-//     opacity: 0
-//   }, 250, function(){
-//     // Animated
-//
-//   });
-// });
-
-
 // On hover show skills
 $(".skill-img__top-div").hover(function(){
   $(".skill-box--frontend").toggleClass("active");
   $(".skills-text").toggleClass("passive");
   $(".skills-container").toggleClass("justify-center");
-
-
 });
 $(".skill-img__mid-div").hover(function(){
   $(".skill-box--tool").toggleClass("active");
   $(".skills-text").toggleClass("passive");
   $(".skills-container").toggleClass("justify-center");
-
-
 });
 $(".skill-img__bottom-div").hover(function(){
   $(".skill-box--backend").toggleClass("active");
   $(".skills-text").toggleClass("passive");
   $(".skills-container").toggleClass("justify-center");
-
 });
 
 
 
-
-// Top
+// Top div on hover animate skill images having ".frontend"
 $(".skill-img__top-div").hover(function(){
   $(".frontend").animate({
     opacity: 1,
@@ -102,17 +79,14 @@ $(".skill-img__top-div").hover(function(){
     // Animated
   });
 });
-
 $(".skill-img__top-div").mouseleave(function(){
-  $(".frontend").animate({
-    opacity: 0
-  }, 1, function(){
+  $(".frontend").animate({ opacity: 0 }, 1, function(){
     // Animated
   });
 });
 
 
-// Mid
+// Middle div on hover animate skill images having ".tool"
 $(".skill-img__mid-div").hover(function(){
   $(".tool").animate({
     opacity: 1,
@@ -121,18 +95,14 @@ $(".skill-img__mid-div").hover(function(){
     // Animated
   });
 });
-
 $(".skill-img__mid-div").mouseleave(function(){
-  $(".tool").animate({
-    opacity: 0
-  }, 1, function(){
+  $(".tool").animate({ opacity: 0 }, 1, function(){
     // Animated
   });
 });
 
 
-// Bottom
-
+// Bottom div on hover animate skill images having ".backend"
 $(".skill-img__bottom-div").hover(function(){
   $(".backend").animate({
     opacity: 1,
@@ -141,21 +111,15 @@ $(".skill-img__bottom-div").hover(function(){
     // Animated
   });
 });
-
 $(".skill-img__bottom-div").mouseleave(function(){
-  $(".backend").animate({
-    opacity: 0
-  }, 1, function(){
+  $(".backend").animate({ opacity: 0 }, 1, function(){
     // Animated
   });
 });
 
 
 
-
-// Activating ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
-
+// In order to make it more understandable, following code is way more commented that it needs to be.
 // When hoverable parts is visible in viewport, do this animation:
 gsap.from(".skill-img__ind-wrapper", { // for "at" class, from -40 of its real position to its real position
   scrollTrigger: ".skill-img__ind-wrapper", // activate animation when "at" class is visible on viewport
@@ -169,19 +133,6 @@ gsap.from(".skill-img__ind-wrapper", { // for "at" class, from -40 of its real p
   /* Solution is putting them into a div, and animating the div itself, not touching the element. */
   stagger:0.2 // if there is more than 1 component having "at", set 0.2 second delay for next one
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -204,9 +155,7 @@ gsap.timeline()
     stagger:0.2
   })
   .from(".hero-contact-nav", { // Hero contact nav animation
-    scrollTrigger: {
-      trigger: ".hero-contact-nav"
-    },
+    scrollTrigger: ".hero-contact-nav",
     y: -40,
     opacity: 0,
     duration: 1,
@@ -215,6 +164,7 @@ gsap.timeline()
   });
 
 
+// Footer contact nav animation
 gsap.from(".footer-contact-nav", {
   scrollTrigger: {
     trigger: ".footer-contact-nav"
@@ -227,11 +177,11 @@ gsap.from(".footer-contact-nav", {
 });
 
 
-
+// skills-text class revealing & hiding animations
 const skillsTextAnimation = gsap.from(".skills-text", {
   // Animation applied to skills-text
   scrollTrigger: {
-    trigger: ".skill-img" // when skill-img is visible on screen
+    trigger: ".skills-text" // when skill-text is visible on screen
   },
   y: -40,
   opacity: 0,
@@ -239,65 +189,62 @@ const skillsTextAnimation = gsap.from(".skills-text", {
   ease:"power1.out",
   stagger:0.2
 });
-
 // An event listener to trigger the animation once more when displayed
 $(".skill-img-wrapper").mouseleave(function() {
-  skillsTextAnimation.restart(); // restart the animation
+  skillsTextAnimation.restart();
+  // restart the animation when hovering on skill-img-wrapper is done.
 });
 
 
+
+// Corresponding skill stack text animation for revealing (Frontend)
 const skillsStackTextAnimationFrontend = gsap.from(".frontend-text", {
-  // animation is applied on .stack-field
-  scrollTrigger: {
-    // when .stack-field is visible on screen
-    trigger: ".frontend-text"
-  },
+  // animation is applied on .frontend-text
+  scrollTrigger: ".frontend-text",
+  // when .frontend-text is visible on screen
   x: 40,
   opacity: 0,
   duration: 0.5,
   ease:"power1.out",
 });
-
 // An event listener to trigger the animation once more when displayed
 $(".skill-img__top-div").hover(function(){
   skillsStackTextAnimationFrontend.restart();
+  // Each hover on ".skill-img__top-div" restarts revaling animation
 });
 
 
-
+// Corresponding skill stack text animation for revealing (Tool)
 const skillsStackTextAnimationTool = gsap.from(".tool-text", {
-  // animation is applied on .stack-field
-  scrollTrigger: {
-    // when .stack-field is visible on screen
-    trigger: ".tool-text"
-  },
+  // animation is applied on .tool-text
+  scrollTrigger: ".tool-text",
+  // when .tool-text is visible on screen
   x: 40,
   opacity: 0,
   duration: 0.5,
   ease:"power1.out",
 });
-
 // An event listener to trigger the animation once more when displayed
 $(".skill-img__mid-div").hover(function(){
   skillsStackTextAnimationTool.restart();
+  // Each hover on ".skill-img__mid-div" restarts revaling animation
 });
 
 
+// Corresponding skill stack text animation for revealing (Backend)
 const skillsStackTextAnimationBackend = gsap.from(".backend-text", {
-  // animation is applied on .stack-field
-  scrollTrigger: {
-    // when .stack-field is visible on screen
-    trigger: ".backend-text"
-  },
+  // animation is applied on .backend-text
+  scrollTrigger: ".backend-text",
+  // when .backend-text is visible on screen
   x: 40,
   opacity: 0,
   duration: 0.5,
   ease:"power1.out",
 });
-
 // An event listener to trigger the animation once more when displayed
 $(".skill-img__bottom-div").hover(function(){
   skillsStackTextAnimationBackend.restart();
+  // Each hover on ".skill-img__bottom-div" restarts revaling animation
 });
 
 
