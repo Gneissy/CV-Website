94eaea2521f8e2c42df20fd64049fd21999676c0
Gneissy <ahmetserteserr@gmail.com>
2023-03-29 06:17:18 +0300

    Remove address bar on load

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ gsap.registerPlugin(ScrollTrigger); // Activating ScrollTrigger
 
 
 
-
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
 
 
 
